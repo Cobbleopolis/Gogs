3848537de7d20aab2cc2438a7fc81ec2bd78ef2f
Cobbleopolis <cobbleopolis@gmail.com>
2016-05-16 11:35:17 -0400

    Fixed conditionals for empty fields in ProfileFragment

--- app/src/main/scala/com/cobble/gogs/app/fragment/ProfileFragment.scala
+++ app/src/main/scala/com/cobble/gogs/app/fragment/ProfileFragment.scala
@@ -27,12 +27,12 @@ class ProfileFragment extends CobbleListFragment(R.layout.frag_profile) {
 				mRepo.put("head", fields(i))
 				mRepo.put("sub", i match {
 					case 0 => if(user.id != -1) user.id.toString else "N/A"
-					case 1 => if(user.username != null) user.username else "N/A"
-					case 2 => if(user.fullName != null) user.fullName else "N/A"
-					case 3 => if(user.email != null) user.email else "N/A"
-					case 4 => if(user.avatarUrl != null) user.avatarUrl else "N/A"
-					case 5 => if(user.website != null) user.website else "N/A"
-					case 6 => if(user.location != null) user.location else "N/A"
+					case 1 => if(user.username != null && !user.username.isEmpty) user.username else "N/A"
+					case 2 => if(user.fullName != null && !user.fullName.isEmpty) user.fullName else "N/A"
+					case 3 => if(user.email != null && !user.email.isEmpty) user.email else "N/A"
+					case 4 => if(user.avatarUrl != null && !user.avatarUrl.isEmpty) user.avatarUrl else "N/A"
+					case 5 => if(user.website != null && !user.website.isEmpty) user.website else "N/A"
+					case 6 => if(user.location != null && !user.location.isEmpty) user.location else "N/A"
 					case _ => "N/A"
 				})
 				listItems.add(mRepo)
